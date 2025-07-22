d9e0b72f8dc942c981ba6230345f75de072ac46f
colinscz <19342760+colinscz@users.noreply.github.com>
2025-07-22 08:23:43 +0200

    feat: prepare for ssr

--- next-pages-app/next.config.ts
+++ next-pages-app/next.config.ts
@@ -1,4 +1,18 @@
 import type { NextConfig } from "next";
+// import stencilSSR from '@stencil/ssr/next';
+
+ /** @type {import('next').NextConfig} */
+/* const nextConfig = {};
+
+export default stencilSSR({
+  module: import('@six-group/ui-library-react'),
+  from: '@six-group/ui-library-react',
+  hydrateModule: import('@six-group/ui-library/hydrate'),
+  serializeShadowRoot: {
+    scoped: ['my-counter'],
+    default: 'declarative-shadow-dom',
+  },
+})(nextConfig);  */
 
 const nextConfig: NextConfig = {
   /* config options here */
